d7dc9711d906a80ef880d1b111d748d3e4d3fc4c
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-05-29 21:53:54 +0200

    zmienne w zadaniu

---
+++ script.js
@@ -0,0 +1,4 @@
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
