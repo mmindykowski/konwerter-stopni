4414b0566d0952ebdaa15dddab9f9e447131e070
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-05-30 21:06:37 +0200

    funkcja conversion

--- script.js
+++ script.js
@@ -22,14 +22,26 @@ const swap = () => {
 const fahrToCel = () => {
   fahrenheit = converter.value * 1.8 + 32;
   result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
-  converter.value = ''
+  converter.value = "";
 };
 
 const celToFahr = () => {
-    celsius = (converter.value - 32) / 1.8;
-    result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
-    converter.value = ''
-  };
+  celsius = (converter.value - 32) / 1.8;
+  result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
+  converter.value = "";
+};
+
+const conversion = () => {
+  if (converter.value !== "") {
+    if (one.textContent === "°C") {
+      fahrToCel();
+    } else {
+      celToFahr();
+    }
+  } else {
+    result.textContent = "Musisz podać jakąś wartość!";
+  }
+};
 
 changeBtn.addEventListener("click", swap);
-convBtn.addEventListener("click", celToFahr);
+convBtn.addEventListener("click", conversion);
