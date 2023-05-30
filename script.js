58c2ef067477767d636808b7a670415248b92d7f
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-05-30 21:00:27 +0200

    kolejny etap zadania

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-const converter = document.querySelector('#converter');
-const result = document.querySelector('.result');
-const convBtn = document.querySelector('.conv');
-const resetBtn = document.querySelector('.reset');
-const changeBtn = document.querySelector('.change');
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
+const converter = document.querySelector("#converter");
+const result = document.querySelector(".result");
+const convBtn = document.querySelector(".conv");
+const resetBtn = document.querySelector(".reset");
+const changeBtn = document.querySelector(".change");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+
+let fahrenheit;
+let celsius;
 
 const swap = () => {
-    if(one.textContent === '°C') {
-        one.textContent = '°F'
-        two.textContent = '°C'
-    } else {
-        one.textContent = '°C'
-        two.textContent = '°F'
-    }
-}
+  if (one.textContent === "°C") {
+    one.textContent = "°F";
+    two.textContent = "°C";
+  } else {
+    one.textContent = "°C";
+    two.textContent = "°F";
+  }
+};
 
+const fahrToCel = () => {
+  fahrenheit = converter.value * 1.8 + 32;
+  result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`;
+  converter.value = ''
+};
 
-changeBtn.addEventListener('click', swap);
+const celToFahr = () => {
+    celsius = (converter.value - 32) / 1.8;
+    result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`;
+    converter.value = ''
+  };
 
+changeBtn.addEventListener("click", swap);
+convBtn.addEventListener("click", celToFahr);
